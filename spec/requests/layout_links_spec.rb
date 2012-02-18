4484c7f3fa8e5517d44fe8f31f5101e85c09b95c
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-02-18 22:09:14 +0000

    integration tests pass

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -14,12 +14,12 @@ describe "LayoutLinks" do
 
   it "should have a Help page at '/help'" do
     get '/help'
-    response should have_selector('title', :content => "Help")
+    response.should have_selector('title', :content => "Help")
   end
 
   it "should have an About page at '/about'" do
     get '/about/'
-    response should have_selector('title', :content => "About")
+    response.should have_selector('title', :content => "About")
   end
 
 end
