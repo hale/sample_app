5034c178db2dcd1aa658447c2f6998a8a876a8fc
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-02-19 15:51:30 +0000

    completed user validations, section 6.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
-class User < ActiveRecord::Base
-  attr_accessible :name, :email
-end
-
 # == Schema Information
 #
 # Table name: users
@@ -12,4 +8,21 @@ end
 #  created_at :datetime        not null
 #  updated_at :datetime        not null
 #
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+
+  email_regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
+  validates :name, :presence => true,
+                   :length   => { :maximum => 50 } 
+           
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false } 
+           
+
+end
+
 
